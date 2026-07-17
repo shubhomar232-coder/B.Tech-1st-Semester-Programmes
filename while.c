6343a7d391352b806107cfdcf0f211522831f09c
shubhomar232-coder <shubhomar232@gmail.com>
2026-07-17 15:19:27 +0530

    Add while loop to print numbers from 0 to n
    Semester-1/C-Programming/sum_n_numbers.cCommit Message: Add sum_n_numbers.c for calculating total sumExtended Description: Implements a C program that asks the user how many numbers they want to add, takes those integers as input sequentially using a loop, and prints their total sum.

---
+++ while.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int n;
+  printf("enter n number : ");
+  scanf("%d \n" , &n);
+  int i = 0;
+  while (i<=n){
+    printf("%d \n" , i);
+    i++;
+  }    
+    return 0;
+}
